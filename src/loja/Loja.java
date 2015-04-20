596fba3f3e3b50019e1b5e7a1b09eaadc8d048ac
isvaldo <isvaldo.fernandes@gmail.com>
2015-04-19 22:52:15 -0300

    Criando carro, parte da tarefa #4

--- src/loja/Loja.java
+++ src/loja/Loja.java
@@ -17,4 +17,72 @@ public class Loja {
 	private String nome;
 	private Carro[] carros;
 	private Motocicleta[] motocicletas;
+	private static final int CARROESTOQUE = 10;
+	private static final int MOTOESTOQUE = 10;
+
+	/**
+	 * Construtor de Loja
+	 * Inicia os valores default para arrays primitivas
+	 * */
+	Loja(){
+		this.carros = new Carro[CARROESTOQUE];
+	}
+
+	/**
+	 * adicionaCarro Adiciona objeto carro a lista
+	 * primitiva de carros
+	 * @param carro, objeto carro
+	 * @param p, posição a ser inserida, caso exista é sobrescrito
+	 * */
+	
+	public void adicionaCarro(Carro carro, int p) {
+		// TODO filtro de erros para p invalida
+		this.carros[p] = carro; 
+	}
+	
+	
+	
+	// Get e Set
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Carro[] getCarros() {
+		return carros;
+	}
+
+	public void setCarros(Carro[] carros) {
+		this.carros = carros;
+	}
+
+	public Motocicleta[] getMotocicletas() {
+		return motocicletas;
+	}
+
+	public void setMotocicletas(Motocicleta[] motocicletas) {
+		this.motocicletas = motocicletas;
+	}
+
+	public static int getCarroestoque() {
+		return CARROESTOQUE;
+	}
+
+	public static int getMotoestoque() {
+		return MOTOESTOQUE;
+	}
+	// fim get  e set
+	
+	
 }
