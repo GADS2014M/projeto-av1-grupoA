f9421c92fa61324d5205df5d07d516aa5a115519
isvaldo <isvaldo.fernandes@gmail.com>
2015-04-23 21:39:40 -0300

    Criando menu

--- src/loja/Loja.java
+++ src/loja/Loja.java
@@ -1,17 +1,8 @@
 package loja;
 
 import java.util.ArrayList;
-import java.util.Scanner;
-
 import loja.veiculo.Carro;
 import loja.veiculo.Motocicleta;
-import loja.veiculo.types.Cambio;
-import loja.veiculo.types.Chassi;
-import loja.veiculo.types.Cor;
-import loja.veiculo.types.Modelo;
-import loja.veiculo.types.Montadora;
-import loja.veiculo.types.Tipo;
-
 public class Loja {
 	/**
 	 * Class Loja Controle de estoque
@@ -26,15 +17,17 @@ public class Loja {
 	private String nome;
 	private ArrayList<Carro> carros = new ArrayList<>();
 	private ArrayList<Motocicleta> motocicletas = new ArrayList<>();
-	private Scanner sc = new Scanner(System.in);
-
+	
+	
+	
+	
 	/**
 	 * adicionaCarro Adiciona objeto carro a lista
 	 * primitiva de carros
 	 * @param carro, objeto carro
 	 * @param p, posição a ser inserida, caso exista é sobrescrito
 	 * */
-	private void adicionaCarro(Carro carro) {
+	public void adicionaCarro(Carro carro) {
 		// TODO filtro de erros para p invalida
 		this.carros.add(carro);
 	}
@@ -46,248 +39,10 @@ public class Loja {
 	 * @param p, posição a ser inserida, caso exista é sobrescrito
 	 * */
 	
-	private void adicionaMoto(Motocicleta moto) {
+	public void adicionaMoto(Motocicleta moto) {
 		// TODO filtro de erros para p invalida
 		this.motocicletas.add(moto);
 	}
-	
-	public void adicionaCarroByTerminal(){
-		Carro carro = getCarroValues();
-		adicionaCarro(carro);
-	}
-
-	/**
-	 * getCarroValues, coleta todos itens de carro
-	 * @return Objeto carro
-	 */
-	private Carro getCarroValues() {
-		Chassi chassi = getChassi();
-		Montadora montadora = getMontadora();
-		Modelo modelo = getModelo();
-		Tipo tipo = getTipo();
-		Cor cor = getCor();
-		System.out.println("Qual motorização?");
-		float motorizacao = this.sc.nextFloat();
-		Cambio cambio = getCambio();
-		System.out.println("Qual valor do carro?");
-		float valor = this.sc.nextFloat();
-		Carro carro =  Carro.criaCarro(chassi, montadora, modelo, tipo, cor, motorizacao, cambio, valor);
-		return carro;
-	}
-	
-	public void adicionaMotoByTerminal(){
-		Motocicleta moto = getMotoValues();
-		adicionaMoto(moto);
-	}
-
-	/**
-	 * getMotoValues, coleta todos itens de moto
-	 * @return objeto moto
-	 */
-	private Motocicleta getMotoValues() {
-		Chassi chassi = getChassi();
-		Montadora montadora = getMontadora();
-		Modelo modelo = getModelo();
-		Tipo tipo = getTipo();
-		Cor cor = getCor();
-		System.out.println("Qual a capacidade do tanque?");
-		byte capacidadeDoTanque  = (byte) this.sc.nextInt();
-		System.out.println("Qual a cilindadra?");
-		int cilindrada = this.sc.nextInt();
-		System.out.println("Qual valor da moto?");
-		float valor = this.sc.nextFloat();
-		Motocicleta moto = Motocicleta.criaMoto(chassi, montadora, modelo, tipo, cor, cilindrada, capacidadeDoTanque, valor);
-		return moto;
-	}
-
-	/**
-	 * getChassi
-	 * Mostra uma lista valida de chassi
-	 * @return Objeto chassi
-	 */
-	private Chassi getChassi() {
-		//@TODO colocar filtros
-		System.out.println("Escolha um chassi disponivel");
-		this.mostraOpcao(Chassi.class);
-		System.out.println("Informe uma ID(numeral)");
-		return Chassi.valueOf(getGenericsEnumByCode(Chassi.class,this.sc.nextInt()));
-		
-	}
-	
-	/**
-	 * getMontadora
-	 * Mostra uma lista valida de Montadora
-	 * @return Objeto Montadora
-	 */
-	private Montadora getMontadora() {
-		//@TODO colocar filtros
-		System.out.println("Escolha uma Montadora disponivel");
-		this.mostraOpcao(Montadora.class);
-		System.out.println("Informe uma ID(numeral)");
-		return Montadora.valueOf(getGenericsEnumByCode(Montadora.class,this.sc.nextInt()));
-		
-	}
-	
-	/**
-	 * getModelo
-	 * Mostra uma lista valida de Modelo
-	 * @return Objeto Modelo
-	 */
-	private Modelo getModelo() {
-		//@TODO colocar filtros
-		System.out.println("Escolha um Modelo disponivel");
-		this.mostraOpcao(Modelo.class);
-		System.out.println("Informe uma ID(numeral)");
-		return Modelo.valueOf(getGenericsEnumByCode(Modelo.class,this.sc.nextInt()));
-	}
-	
-	/**
-	 * getTipo
-	 * Mostra uma lista valida de Tipo
-	 * @return Objeto Tipo
-	 */
-	private Tipo getTipo() {
-		//@TODO colocar filtros
-		System.out.println("Escolha um Tipo disponivel");
-		this.mostraOpcao(Tipo.class);
-		System.out.println("Informe uma ID(numeral)");
-		return Tipo.valueOf(getGenericsEnumByCode(Tipo.class,this.sc.nextInt()));
-	}
-	
-	/**
-	 * getCor
-	 * Mostra uma lista valida de Cor
-	 * @return Objeto Cor
-	 */
-	private Cor getCor() {
-		//@TODO colocar filtros
-		System.out.println("Escolha uma Cor disponivel");
-		this.mostraOpcao(Cor.class);
-		System.out.println("Informe uma ID(numeral)");
-		return Cor.valueOf(getGenericsEnumByCode(Cor.class,this.sc.nextInt()));
-	}
-	
-	/**
-	 * getCambio
-	 * Mostra uma lista valida de Cambio
-	 * @return Objeto Cambio
-	 */
-	private Cambio getCambio() {
-		//@TODO colocar filtros
-		System.out.println("Escolha um Cambio disponivel");
-		this.mostraOpcao(Cambio.class);
-		System.out.println("Informe uma ID(numeral)");
-		return Cambio.valueOf(getGenericsEnumByCode(Cambio.class,this.sc.nextInt()));
-	}
-	
-	
-	public static void main(String[] args) {
-		//new Loja().adicionaCarroByTerminal();
-		//new Loja().adicionaMotoByTerminal();
-	}
-
-	/**
-	 * mostraOpcao mostra opções de um enum de forma generica
-	 * @param enumClass classe de um enum
-	 */
-	private <T extends Enum<T>> void mostraOpcao(Class<T> enumClass) {
-	    for (Enum<T> item : enumClass.getEnumConstants()) {
-	    	// Mostra valor -> representacao
-	        System.out.println(item.ordinal()+"->"+item.toString());
-	    }
-	}
-
-	
-	/**
-	 * getGenericsEnumByCode retorna um enum generico pelo codigo
-	 * @param enumClass classe de um enum
-	 * @param code valor que apontado pelo enum
-	 */
-	private  <T extends Enum<T>> String getGenericsEnumByCode(Class<T> enumClass,int code) {
-	    for (Enum<T> item : enumClass.getEnumConstants()) {
-	    	if (item.ordinal() ==code){
-	    		return item.toString();
-	    	}
-	    }
-	    //@TODO adicionar filtros
-	    System.out.println("Valor não encotnrado");
-	    return "Erro";
-	}
-	
-	/**/
-	private ArrayList<Carro> buscaCarroByChassi() {
-		Chassi chassi = getChassi();
-		ArrayList<Carro> resultadoBuscaCarro = new ArrayList<>();
-		for (Carro carro: this.carros){
-			if(carro.getChassi() == chassi){
-				resultadoBuscaCarro.add(carro);
-			}
-		}
-		
-		return resultadoBuscaCarro;
-	}
-	
-	private ArrayList<Motocicleta> buscaMotoByChassi() {
-		Chassi chassi = getChassi();
-		ArrayList<Motocicleta> resultadoBuscaMoto = new ArrayList<>();
-		for (Motocicleta moto: this.motocicletas){
-			if(moto.getChassi() == chassi){
-				resultadoBuscaMoto.add(moto);
-			}
-		}
-		
-		return resultadoBuscaMoto;
-	}
-	
-	
-	
-	
-	/**
-	 * pesquisaCarroByTerminal, Pesquisa um carro usando todos os atributos
-	 * @return ArrayList de carro com todos os resultados
-	 * */
-	private ArrayList<Carro> pesquisaCarroByTerminal() {
-		ArrayList<Carro> resultadoPesquisa = new ArrayList<>();
-		Carro carroTeste =  getCarroValues();
-		for (Carro carro : this.carros) {
-			if (carro.equals(carroTeste))
-				resultadoPesquisa.add(carro);
-		}
-		//@TODO Adicionar filtros, null
-		return resultadoPesquisa;
-	}
-	
-	/**
-	 * pesquisaMotoByTerminal, Pesquisa uma moto usando todos os atributos
-	 * @return ArrayList de moto com todos os resultados
-	 * */
-	private ArrayList<Motocicleta> pesquisaMotoByTerminal() {
-		ArrayList<Motocicleta> resultadoPesquisa = new ArrayList<>();
-		Motocicleta motoTeste =  getMotoValues();
-		for (Motocicleta moto : this.motocicletas) {
-			if (moto.equals(motoTeste))
-				resultadoPesquisa.add(moto);
-		}
-		//@TODO Adicionar filtros, null
-		return resultadoPesquisa;
-	}
-	
-	
-	private void listaCarros() {
-		for (Carro carro: this.carros){
-			System.out.println(carro);
-		}
-		
-	}
-	
-	private void listaMoto() {
-		for (Motocicleta moto: this.motocicletas){
-			System.out.println(moto);
-		}
-		
-	}
-
-	
 	
 	
 	// Get e Set
@@ -322,8 +77,6 @@ public class Loja {
 	public void setMotocicletas(ArrayList<Motocicleta> motocicletas) {
 		this.motocicletas = motocicletas;
 	}
-
-	
 	// fim get  e set
 	
 	
---
+++ src/loja/menu/Menu.java
@@ -0,0 +1,17 @@
+package loja.menu;
+
+public interface Menu {
+	
+	/**display
+	 * Mostra na tela o menu
+	 * */
+	void display();
+	
+	/**
+	 * Renova o conteudo da tela, limpando
+	 * ou atualizando
+	 * */
+	void refresh();
+	
+	
+}
